5a44aa8c25a2333c828b89bdb543e2e712281f16
Alexander Slivkov <alexandrslivkov@gmail.com>
2026-02-21 17:03:26 +0300

    Двигаюсь дальше

---
+++ 06_logic/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	/*
+		возвращается bool
+		==
+		!=
+		>
+		<
+		>=
+		<=
+
+		!
+		&&
+		||
+
+	*/
+	x := 1
+	y := 0
+	fmt.Println(x == 1 || y == 1, x == 1 && y == 1)
+}
